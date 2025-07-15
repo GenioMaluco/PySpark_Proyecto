57d1a3173fdf18e30a9accfa5a9a6523348868ad
geniomaluco <adrian.arayaaguilar@gmail.com>
2025-07-15 16:11:43 -0600

    Primer Commit: Proyecto PySpark con Docker

---
+++ PySpark_Proyecto/src/main.py
@@ -0,0 +1,15 @@
+﻿from pyspark.sql import SparkSession
+
+def main():
+    spark = SparkSession.builder.appName("DockerSpark").getOrCreate()
+    
+    # Ejemplo básico
+    data = [("Python", 1), ("Spark", 2), ("Docker", 3)]
+    df = spark.createDataFrame(data, ["Tecnología", "Ranking"])
+    df.show()
+
+    # Guardar resultados (opcional)
+    df.write.csv("/data/output", mode="overwrite")
+
+if __name__ == "__main__":
+    main()
